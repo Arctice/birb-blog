0b65ff329aea11c1b5bb6ca7b6f6a63357877caa
arc <naurthal0@gmail.com>
2017-04-16 18:59:24 +0200

    submodule theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,5 +31,12 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = False
 
+THEME = './themes/voce/'
+
+PLUGIN_PATHS = ['./themes/voce/plugins/']
+PLUGINS = ['assets']
+
+#JINJA_ENVIRONMENT = {}
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
