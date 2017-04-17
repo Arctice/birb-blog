175113b60d1a168c5a9ddc2b0e86a22fccc08898
Arctice <naurthal0@gmail.com>
2017-04-17 22:05:25 +0200

    importing from pelicanconf fails on travis

--- publishconf.py
+++ publishconf.py
@@ -1,10 +1,42 @@
-from pelicanconf import *
+AUTHOR = 'birbs'
+SITENAME = 'birb plays games'
+SITEURL = 'http://birb.uk'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+DEFAULT_LANG = 'English'
 
 #FEED_ALL_ATOM = 'feeds/all.atom.xml'
 #CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
-#GOOGLE_ANALYTICS = ""
-GOOGLE_ANALYTICS_ID = None
-GOOGLE_ANALYTICS_PROP = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+#GOOGLE_ANALYTICS = ""
+GOOGLE_ANALYTICS_ID = None
+GOOGLE_ANALYTICS_PROP = None
+
+# Blogroll
+LINKS = (('Home', SITEURL+'/index.html'),
+        ('Uni', 'https://twitter.com/Unis_verse'),
+        ('Arc', 'https://twitter.com/rainlife__'),
+        )
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 4
+
+THEME = './themes/voce/'
+
+PLUGIN_PATHS = ['./themes/voce/plugins/']
+PLUGINS = ['assets']
+
+USER_LOGO_URL = 'http://i.imgur.com/3ANGARn.png'
+MANGLE_EMAILS = True
+FUZZY_DATES = True  # uses JS to display fancy dates
+
+DISPLAY_CATEGORIES_ON_MENU = True
+DISPLAY_PAGES_ON_MENU = True
+DEFAULT_CATEGORY = 'Misc'
+
