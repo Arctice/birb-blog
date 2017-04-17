359e3d90bea07754d0f8a4186cb43ceb6bce73de
Arctice <naurthal0@gmail.com>
2017-04-17 22:23:34 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -7,14 +7,25 @@ PATH = 'content'
 TIMEZONE = 'Europe/London'
 DEFAULT_LANG = 'English'
 
-#FEED_ALL_ATOM = 'feeds/all.atom.xml'
-#CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+AUTHOR_FEED_ATOM = 'feeds/by_%s.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss'
+CATEGORY_FEED_RSS = 'feeds/%s.rss'
+AUTHOR_FEED_RSS = 'feeds/by_%s.rss'
 #GOOGLE_ANALYTICS = ""
-GOOGLE_ANALYTICS_ID = None
-GOOGLE_ANALYTICS_PROP = None
+#GOOGLE_ANALYTICS_ID = None
+#GOOGLE_ANALYTICS_PROP = None
+
+# Markdown extensions
+MARKDOWN = {'extension_configs':{
+                'markdown.extensions.footnotes': {},
+                'markdown.extensions.tables': {},
+                'markdown.extensions.nl2br': {},
+                'markdown.extensions.sane_lists': {},
+                'markdown.extensions.toc': {},
+                }
+        }
 
 # Blogroll
 LINKS = (('Home', SITEURL+'/index.html'),
